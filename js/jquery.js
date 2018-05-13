58392b887b1900bc1062a78fff7833233e6a6383
Soontrax <32308953+Soontrax@users.noreply.github.com>
2018-05-13 12:34:44 +0200

    Add files via upload

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,17 @@
-$(window).scroll(function() {
-    var wintop = $(window).scrollTop(), docheight = $('article').height(), winheight = $(window).height();
-    console.log(wintop);
-    var totalScroll = (wintop/(docheight-winheight))*100;
-    console.log("total scroll" + totalScroll);
-    $(".KW_progressBar").css("width",totalScroll+"%");
-  });
+//Función Para el scrool bar indicator
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+ var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+ var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+ var scrolled = (winScroll / height) * 100;
+ document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+//Función de Preloader de Página
+window.onload = function(){
+  var contenedor = document.getElementById('contenedor_carga');
+  
+  contenedor.style.visibility = 'hidden';
+  contenedor.style.opacity = '0';
+}
