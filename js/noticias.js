29b434a3c2fb32a9aaf9fe220009af15438fd500
Soontrax <32308953+Soontrax@users.noreply.github.com>
2018-05-26 17:57:47 +0200

    Add files via upload

--- js/noticias.js
+++ js/noticias.js
@@ -22,19 +22,16 @@ window.onload = function () {
     datatype: "jsonp",
     type: "GET",
     data: {},
-    url: "https://tennis-warehouse.firebaseio.com/News/.json",
+    url: "https://rawgit.com/Soontrax/WEB_Boostrap-JQuery/master/json/PrimerasNoticias.json",
     success: function (data) {
       console.log(data);
       var html = "";
-      var contador = 1;
-      $.each(data, function (idx, item) {
-        var contador = 1;
+      $.each(data.News, function (idx, item) {
         //Añadimos las etiquetas para cada 2 objetos que pasen
         if (idx % 2 == 0) {
           html += '</br><div style="display:none" class="news row">';
         }
         html += '<div class="col-md-4">' +
-          '<a href="pagina' + contador + '.html"></a>' +
           '<div class="thumbnail">' +
           '<div class="caption">' +
           '<div class="ribbon">' +
@@ -43,7 +40,9 @@ window.onload = function () {
           '</span>' +
           '</div>' +
           '<h3 class="text-align: center">' +
+          '<a href="pagina1.html?index=' + idx + '">' +
           '<b>' + item.Subtitle + '</b>' +
+          '</a>' +
           '</h3>' +
           '<img src="' + item.Image + '" class="img-responsive">' +
           '</div>' +
@@ -56,14 +55,11 @@ window.onload = function () {
           '</div>' +
           '</div>';
         //Cerramos la etiqueta del div al final de cada 2 objetos en el JSON
-        contador++;
         if (idx % 2 != 0) {
           html += '</div>';
         }
-        
       });
-
-      if (data.length % 2 != 0) {
+      if (data.News.length % 2 != 0) {
         html += '</div>';
       }
 
@@ -72,22 +68,74 @@ window.onload = function () {
       for (var i = 0; i <= 2; i++) {
         $($('.news')[i]).css('display', 'block');
       }
+      $.ajax({
+        datatype: "jsonp",
+        type: "GET",
+        data: {},
+        url: "https://rawgit.com/Soontrax/WEB_Boostrap-JQuery/master/json/SegundasNoticias.json",
+        success: function (data) {
+          console.log(data);
+          var html = "";
+          $.each(data.News, function (idx, item) {
+            //Añadimos las etiquetas para cada 2 objetos que pasen
+            if (idx % 2 == 0) {
+              html += '</br><div style="display:none" class="news row">';
+            }
+            html += '<div class="col-md-4">' +
+              '<div class="thumbnail">' +
+              '<div class="caption">' +
+              '<div class="ribbon">' +
+              '<span>' +
+              '<span>' + item.Title + '</span>' +
+              '</span>' +
+              '</div>' +
+              '<h3 class="text-align: center">' +
+              '<a href="pagina1.html?index=' + idx + '">' +
+              '<b>' + item.Subtitle + '</b>' +
+              '</a>' +
+              '</h3>' +
+              '<img src="' + item.Image + '" class="img-responsive">' +
+              '</div>' +
+              '<div class="caption">' +
+              '<p class="text-justify">' + item.Description + '</p>' +
+              '<p class="text-right">' +
+              '<em>' + item.Date + '</em>' +
+              '</p>' +
+              '</div>' +
+              '</div>' +
+              '</div>';
+            //Cerramos la etiqueta del div al final de cada 2 objetos en el JSON
+            if (idx % 2 != 0) {
+              html += '</div>';
+            }
+          });
+          if (data.News.length % 2 != 0) {
+            html += '</div>';
+          }
+
+          $('#container').append(html);
+
+          for (var i = 0; i <= 2; i++) {
+            $($('.news')[i]).css('display', 'block');
+          }
+        }
+      });
     }
   });
-}
 
-//Funcion Para cambiar de imagen cada 3s
-var timer = setInterval(Funcion,2000)
-var counter = 0;
-var a = [];
-a [0] = "img/anuncio1.jpg";
-a [1] = "img/anuncio2.jpg"
-a [2] = "img/anuncio3.jpg"
+  //Funcion Para cambiar de imagen cada 3s
+  var timer = setInterval(Funcion, 2000)
+  var counter = 0;
+  var a = [];
+  a[0] = "img/anuncio1.jpg";
+  a[1] = "img/anuncio2.jpg"
+  a[2] = "img/anuncio3.jpg"
 
-function Funcion(){
-	$("#imagen").attr("src",a[counter]);
-	counter++;
-	if (counter==3) {
-		counter=0;
-	}
+  function Funcion() {
+    $("#imagen").attr("src", a[counter]);
+    counter++;
+    if (counter == 3) {
+      counter = 0;
+    }
+  }
 }
